42bd59f0b2c98a64a92aca1acf0115f98fc36466
fabio.forno <fabio.forno@9c90f44f-7f2b-0410-bd6b-8d357d4927a6>
2008-01-13 10:54:28 +0000

    upgraded the tap plugin system

---
+++ twisted/plugins/proxy65.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2008 Fabio Forno
+
+from twisted.scripts.mktap import _tapHelper
+
+Proxy65 = _tapHelper(
+        "Proxy65",
+        "proxy65.proxy65",
+        "XEP 65 Bytestream Proxy Component",
+        "proxy65")
