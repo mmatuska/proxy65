bec5e74bad993f5f0b3d4a9a9864f87ae93b9f85
fabio.forno@gmail.com <fabio.forno@gmail.com@9c90f44f-7f2b-0410-bd6b-8d357d4927a6>
2008-12-14 12:50:35 +0000

    1) removed old files

--- twisted/plugins/proxy65.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2008 Fabio Forno
-
-from twisted.scripts.mktap import _tapHelper
-
-Proxy65 = _tapHelper(
-        "Proxy65",
-        "proxy65.tap",
-        "XEP 65 Bytestream Proxy Component",
-        "proxy65")
